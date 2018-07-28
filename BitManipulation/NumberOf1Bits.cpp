0f8e12409bd05291f2643f6e4daa6990a40dd69d
Nishant Marwah <nishantmarwah.10@gmail.com>
2018-07-28 21:38:56 +0530

    bit Manipulation problems added

---
+++ BitManipulation/NumberOf1Bits.cpp
@@ -0,0 +1,20 @@
+// Write a function that takes an unsigned integer and returns the number of 1 bits it has.
+
+// Example:
+
+// The 32-bit integer 11 has binary representation
+
+// 00000000000000000000000000001011
+// so the function should return 3.
+
+
+
+int Solution::numSetBits(unsigned int A) {
+    
+    int numOnes = 0;
+    while(A!=0){
+        A = A & (A-1);
+        numOnes++;
+    }
+    return numOnes;
+}
---
+++ Math/findFactors.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{	
+	int n;
+	vector<int> v;
+	cin >> n;
+	v = findFactors(n);
+	printVector(v);
+	return 0;
+}
+
+vector<int> findFactors(int n){
+	for(int i=0;i<)
+}
+
+void printVector(vector<int> v){
+	for(int n:v){
+		cout << n <<"\n";
+	}
+
+}
+
